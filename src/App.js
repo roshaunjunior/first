dfe57d6106d0cbed2b6a4de936aa6e7c11af24f7
Talha Roshaun <talharoshaun@gmail.com>
2022-12-22 17:54:14 +0500

    pedro 3rd video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+//import {User} from './User'
 function App() {
+  
+  const planets = [
+    {name: "Earth" ,isGasPlanet : false } , 
+     {name : "Mars" , isGasPlanet : false } ,
+     {name : "Venus" , isGasPlanet : true} ,
+     {name : "Jupiter" , isGasPlanet : false} ,
+     {name : "Neptune" , isGasPlanet : true} ,
+     {name : "Uranus" , isGasPlanet : true}
+] ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+     {planets.map(
+      (planet , key) => planet.isGasPlanet && <h1>{planet.name}</h1> 
+      
+
+     )}
     </div>
   );
 }
+
+
+
 
 export default App;
