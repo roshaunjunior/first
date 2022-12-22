6dc9b82d0acb0a7846364515fb54364e49d824de
Talha Roshaun <talharoshaun@gmail.com>
2022-12-22 19:36:50 +0500

    Exercise To Make Counter using useState

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import {useState} from 'react' ;
 //import {User} from './User'
 function App() {
   
-  const [textColor ,setTextColor] = useState("green") ;
-  // const showOrHide = () => {
-  //   setText(!text)
+  const [count ,setCount] = useState(0) ;
+  const increament = () => {
+    setCount(count + 1)
+  }
+
+  const decreament = () => {
+    setCount(count - 1 )
+  }
+
+  // const setZero = () => {
+  //   setCount(0 )
   // }
   return (
     <div className="App"> 
-     <button onClick={() => {
-      setTextColor(textColor === "green" ? "red" : "green") 
-     }}> Change Color  </button>
-     <h1 style={{color : textColor}}>My Name is Talha </h1> 
+    <button onClick={increament}> Increament </button>
+    <button onClick={decreament}> Decreament </button>
+    <button onClick={()=>setCount(0)}> Set to Zero </button>
+      {count}
     </div>
   );
 }
